ca31f7f8343f1dfb53a785af4b1d4969e017132c
Nicholas070 <kid0807@naver.com>
2024-11-19 14:48:03 +0900

    App class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,7 @@
 package org.example;
 
-//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
-// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         App app = new App();
@@ -10,11 +10,19 @@ public class Main {
 }
 
 class App {
-public void run() {
-    System.out.println("== 명언앱 ==");
+    public void run() {
+        System.out.println("== 명언 앱 ==");
 
-            System.out.print("명언");
-}
-}
+        Scanner scanner = new Scanner(System.in);
 
-Class App
+        while ( true) {
+            System.out.print("명령) ");
+            String cmd = scanner.nextLine();
+            if (cmd.equals("종료")) {
+                break;
+            }
+        }
+
+        scanner.close();
+    }
+}
