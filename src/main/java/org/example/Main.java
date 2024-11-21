dd82b73cda894825286c22bc65d3108477d96a75
Nicholas070 <kid0807@naver.com>
2024-11-21 17:56:21 +0900

    WiseSaying constructor

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,14 +15,50 @@ class App {
 
         Scanner scanner = new Scanner(System.in);
 
+        int lastid = 0;
+        WiseSaying lastwiseSaying=null;
+
+
         while ( true) {
             System.out.print("명령) ");
             String cmd = scanner.nextLine();
             if (cmd.equals("종료")) {
                 break;
             }
+            else if (cmd.equals("등록")) {
+                System.out.print("명언 : ");
+                String content = scanner.nextLine();
+                System.out.print("작가 : ");
+                String author = scanner.nextLine();
+                int id =++lastid;
+
+                WiseSaying wiseSaying = new WiseSaying();
+                wiseSaying.id = id;
+                wiseSaying.content = content;
+                wiseSaying.author = author;
+
+                lastwiseSaying = wiseSaying;
+
+                System.out.println("%d번 명언이 등록되었습니다.".formatted(id));
+            }
+            else if (cmd.equals("목록")){
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("------------------");
+                System.out.println("%d / %s / %s".formatted(lastwiseSaying.id, lastwiseSaying.author, lastwiseSaying.content));
+            }
         }
 
         scanner.close();
     }
+}
+class WiseSaying{
+    int id;
+    String content;
+    String author;
+
+WiseSaying(int id, String content, String author) {
+    this.id = id;
+    this.cotent = content;
+    this.author = author;
+}
 }
